babeb180c7cd9034aa28db96aeb8cba03384efd5
ha-manel <manelhammouche3@gmail.com>
2022-11-12 12:11:46 +0100

    Add plant controller and actions

---
+++ app/controllers/plant_controller.rb
@@ -0,0 +1,14 @@
+class PlantController < ApplicationController
+  def index
+    render json: {plants: Plant.all}, status: :ok
+  end
+
+  def show
+    @plant = Plant.find(params[:id])
+    if @plant
+      render json: {plant: @plant}, status: :ok
+    else
+      render json: {error: "plant doesn't exist"}, status: :not_found
+    end
+  end
+end
