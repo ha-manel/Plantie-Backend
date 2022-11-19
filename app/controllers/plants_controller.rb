19db2e106618c30ed559eb24a457d200f92c4933
ha-manel <manelhammouche3@gmail.com>
2022-11-19 16:41:25 +0100

    Update index action

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,6 +1,10 @@
 class PlantsController < ApplicationController
   def index
-    render json: {plants: Plant.all}, status: :ok
+    @new = Plant.where(new: true)
+    @discount = Plant.where('discount != price')
+    @indoor = Plant.where(habitat: 'indoor')
+    @outdoor = Plant.where(habitat: 'outdoor')
+    render json: {new: @new, discount: @discount, indoor: @indoor, outdoor: @outdoor}, status: :ok
   end
 
   def show
