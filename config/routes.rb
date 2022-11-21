edd0259bb790e67cf984981daea257b65ffbc9bd
ha-manel <manelhammouche3@gmail.com>
2022-11-21 13:13:20 +0100

    Add authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   # root "articles#index"
   get '/api/v1/plants', to: 'plants#index'
   get '/api/v1/plant/:id', to: 'plants#show'
+
+  post '/api/v1/users', to: 'users#create'
+  get '/api/v1/users/:user_id', to: 'users#show'
+  get '/api/v1/users', to: 'users#index'
+
+  post '/api/v1/login', to: 'sessions#create'
+  delete '/api/v1/logout', to: 'sessions#destroy'
+  get '/api/v1/logged_in', to: 'sessions#is_logged_in?'
 end
