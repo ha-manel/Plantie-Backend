1993599ec03e0a865043e4facdd68a02e15db7a8
ha-manel <manelhammouche3@gmail.com>
2022-11-21 13:13:28 +0100

    Update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
-
-    resource "*",
+    origins 'http://localhost:3001'
+    resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
