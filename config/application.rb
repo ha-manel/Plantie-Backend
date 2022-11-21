a07d7c5829ed58e899ebc18d5cd1f898565ccdb3
ha-manel <manelhammouche3@gmail.com>
2022-11-21 13:13:07 +0100

    Setup session

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module PlantieBackend
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
+  
 end
