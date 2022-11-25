6e17aa90c7e7f396ba8df81641825433bb5ec65f
ha-manel <manelhammouche3@gmail.com>
2022-11-25 19:40:56 +0100

    Add models associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :username, length: { minimum: 4 }
+  validates :email, uniqueness: true
+
+  has_many :wishlists
+  has_many :plants, through: :wishlists
 end
