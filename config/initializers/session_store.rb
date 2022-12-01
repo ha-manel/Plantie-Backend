07a1bd880c6309dea37a0e42f2e5b1068c40d070
Manel <manelhammouche3@gmail.com>
2022-12-01 23:10:08 +0100

    Update session_store config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,1 @@
-if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, {
-    :key => '_your_app_name',
-    :domain => :all,
-    :same_site => :none,
-    :secure => :true,
-    :tld_length => 2
-  }
-else
-  Rails.application.config.session_store :cookie_store, key: '_plantie_backend'
-end
+Rails.application.config.session_store :cookie_store, key: '_plantie_backend'
