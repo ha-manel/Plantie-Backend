1b132670758fbee5f95a9cff3f0af3dd4e28a8f0
Manel <manelhammouche3@gmail.com>
2022-12-04 00:21:18 +0100

    Update session config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.session_store :cookie_store, key: '_plantie_backend'
+  config.session_store :cookie_store, key: '_Plantie'
   config.middleware.use ActionDispatch::Cookies
   config.middleware.use config.session_store, config.session_options
 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: '_plantie_backend'
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, key: '_Plantie', domain: :all
+else
+  Rails.application.config.session_store :cookie_store, key: '_Plantie'
+end
