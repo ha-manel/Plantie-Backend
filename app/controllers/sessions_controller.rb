a82e52530c8cd0c05acd76f0f2ad2169fadd049a
Manel <manelhammouche3@gmail.com>
2022-12-03 23:58:46 +0100

    Fix wishlist bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: session_params[:username].downcase)
     if @user&.authenticate(session_params[:password])
-      @wishlist = Plant.joins(:wishlists).where(wishlists: { user_id: session[:user_id] })
       session[:user_id] = @user.id
+      @wishlist = Plant.joins(:wishlists).where(wishlists: { user_id: session[:user_id] })
       render json: { logged_in: true, user: @user, wishlist: @wishlist }
     else
       render json: { errors: 'no such user, please try again' }, status: :not_found
