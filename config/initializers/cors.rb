c081df98958d1e38bdfdb0034f8a42fca7f8f788
Manel <manelhammouche3@gmail.com>
2022-12-07 12:39:10 +0100

    Update cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://plantie-git-dev-ha-manel.vercel.app/'
+    origins 'http://plantie-git-dev-ha-manel.vercel.app/'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
